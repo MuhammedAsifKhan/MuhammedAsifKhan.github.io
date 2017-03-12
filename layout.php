da7b979ba9b0803895beb618ab786f404149c8fa
Muhammed Asif Khan <muhammed.asifkhan92@gmail.com>
2017-03-12 16:14:08 +0600

    Add files via upload

---
+++ layout.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alpha Catering</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<Div class="menubar">
+<div class="menu-logo">
+<img src="alpha2.png" alt="img">
+</div>
+<div class="menu-content">
+<ul>
+	<li>Home</li>
+	<li>Our Story</li>
+	<li>Menu</li>
+	<li>Services</li>
+	<li>Clients</li>
+	<li>Contact Us</li>
+	<li>Get A Quote!</li>
+</ul>
+</div>
+</Div>
+
+<div class="slider">
+<img src="head2.jpg" alt="img">
+</div>
+
+<div class="blog_section">
+<div class="blog">
+<img src="food1p.jpg" alt="img">
+	<h1>Chicken Roast Made From Organic Herbs & Spices</h1>
+	<p>What do you get if you mix Bangladeshi flavours with an Alpha classic cooked roast? This amazing recipe is what!
+Juicy gravy chicken marinated with cumin, turmeric and garam masala and given a good dose of garlic, chilli and ginger for added kick. Dunked in to a rich gravy sauce and served with homemade chutney. Goes oh so well with polao, kacchi or the old favourite - paratha. Life doesn't get much better than this.</p>
+</div>
+
+<div class="blog2">
+<img src="food2p.jpg" alt="img">
+	<h1>We Are Now Serving At IDLC!</h1>
+	<p>It is two weeks into serving office lunch at IDLC head office! The experience here has been different. IDLC customers want lots of variety every day, but are willing to pay a higher price for it. We are thus experimenting with an "a la carte model" for corporate lunch service. If successful, we can replicate it with other clients and introduce something new to office-going folks everywhere!To get our service in your office, call us today at 01755664455.</p>
+	</div>
+<div class="blog3">
+<img src="food3p.jpg" alt="img">
+	<h1>Meet Our Head Chef - Nicolas Rozario!</h1>
+	<p> His name is Nicolas D. Costa. After working for a decade in the Middle East, he came back to Bangladesh and joined as head chef for the ambassador's residence of the German Embassy. Chef Nicolas loved his work there. As foreign guests often visited the ambassador, he got to make all sorts of continental delicacies. Apple pie, fruit custard trifle, chocolate mousse, lasagna, T-bone steak, quiche etc. and so on. We never heard some of them!</p>
+	</div>
+
+</div>
+</body>
+</html>
